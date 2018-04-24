07189cf7bc4575903993b0f37b7537efd9ec34a9
lewisword <31215614+lewisword@users.noreply.github.com>
2018-04-24 16:40:12 +0800

    Update test.py

--- ammmerzougui/0001/test.py
+++ ammmerzougui/0001/test.py
@@ -6,6 +6,6 @@ import random
 
 def genCode(length):
 	s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
-	return "".join(random.sample(s,length))
+	return "".join(random.sample(s,length))#sample(seq, n) 从序列seq中选择n个随机且独立的元素
 l=input("Enter the length of the random code: ")
 print(genCode(int(l)))
